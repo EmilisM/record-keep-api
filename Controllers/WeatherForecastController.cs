b0e77c7d2d9917425ee23dc226fc3381261acb72
Emilis Maktuėnas <emilis.em4@gmail.com>
2020-04-02 21:57:08 +0300

    Add .net core API scaffold

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace record_keep_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("values");
+        }
+    }
+}
